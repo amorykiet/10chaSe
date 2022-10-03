ab643afa9129599a7a11aab837b6b5a3cd0e2681
amorykiet <amorykiet@gmail.com>
2022-10-03 11:09:04 +0700

    fix bug, add sfx, update speed

--- Character.gd
+++ Character.gd
@@ -1,23 +1,30 @@
-extends KinematicBody2D
+extends Area2D
 
 enum {CHASE,ESCAPE}
 var state : int
 var velocity:= Vector2.ZERO
+var speed: int
+var accelator: int
 signal catch_up(winner)
 
-const escape_speed: int = 400
-const chase_speed: int = 300
-const WINDOW = OS.window_size
-
-var speed: int
-var chase_scale: int = 5
+const START_CHASE_SPEED: int = 300
+const START_ESCAPE_SPEED: int = 650
+const MAX_SPEED: int = 1500
+const ACC_CHASE_SPEED: int = 60
+const ACC_ESCAPE_SPEED: int = 10
+var chase_scale: int = 2
 var escape_scale: int = 1
+const WINDOW = OS.window_size
 
 func _ready():
 	if name == "Black":
+		speed = START_ESCAPE_SPEED
 		state = ESCAPE
+		scale = Vector2.ONE*escape_scale
 	elif name == "White":
+		speed = START_CHASE_SPEED
 		state = CHASE
+		scale = Vector2.ONE*chase_scale
 
 func _physics_process(delta):
 	velocity = Vector2.ZERO
@@ -29,31 +36,48 @@ func _physics_process(delta):
 		velocity.y = -1
 	elif Input.is_action_pressed(String(name).to_lower() + "_move_down"):
 		velocity.y = 1
+
 	if state == CHASE:
-		scale = Vector2.ONE* chase_scale
-		speed = chase_speed
+		scale += Vector2.ONE*delta*30
+		if scale > Vector2.ONE* chase_scale:
+			scale = Vector2.ONE* chase_scale
+		accelator = ACC_CHASE_SPEED
 	elif state == ESCAPE:
-		scale = Vector2.ONE* escape_scale
-		speed = escape_speed
-	move_and_slide(velocity*speed)
-	if position.x > WINDOW.x:
-		position.x = 0
-	elif position.x < 0:
-		position.x = WINDOW.x
-	if position.y > WINDOW.y:
-		position.y = 0
-	elif position.y < 0:
-		position.y = WINDOW.y
-	var collision = get_last_slide_collision()
-	if collision:
-		if state == ESCAPE:
-			visible = false
-		else:
-			emit_signal("catch_up", name)
-		
+		scale -= Vector2.ONE*delta*30
+		if scale < Vector2.ONE* escape_scale:
+			scale = Vector2.ONE* escape_scale
+		accelator = ACC_ESCAPE_SPEED
+	speed += accelator*delta
+	if speed > MAX_SPEED:
+		speed = MAX_SPEED
+	
+	position += velocity*speed*delta
+	
+	position.x = wrapf(position.x, 0, WINDOW.x)
+	position.y = wrapf(position.y, 0, WINDOW.y)
 
 func exchange():
 	if state == ESCAPE:
 		state = CHASE
+		speed = START_CHASE_SPEED
 	elif state == CHASE:
 		state = ESCAPE
+		speed = START_ESCAPE_SPEED
+
+func _collide(area):
+	if state == ESCAPE:
+		$CollisionShape2D.set_deferred("disabled", true)
+		visible = false
+	elif state == CHASE:
+		$Catch_up_sound.play()
+		emit_signal("catch_up", name)
+
+func reset_speed():
+	if name == "Black":
+		speed = START_ESCAPE_SPEED
+		state = ESCAPE
+		scale = Vector2.ONE*escape_scale
+	elif name == "White":
+		speed = START_CHASE_SPEED
+		state = CHASE
+		scale = Vector2.ONE*chase_scale
--- Game.gd
+++ Game.gd
@@ -9,14 +9,19 @@ func _ready():
 
 func _process(delta):
 	if game_state == START:
+		$Black/CollisionShape2D.disabled = false
+		$White/CollisionShape2D.disabled = false
 		$Black.position = $Black_start_point.position
 		$White.position = $White_start_point.position
 		$Black.visible = true
 		$White.visible = true
+		$Black.reset_speed()
+		$White.reset_speed()
 		$HUD.message("Space to Start")
 		$HUD.win_message("")
 		$HUD.visible_game(true)
 		if Input.is_action_just_pressed("ui_select"):
+			$Select_sound.play()
 			game_state = PLAYING
 			$HUD.count_down_start()
 			$HUD.message("")
@@ -33,10 +38,12 @@ func _process(delta):
 			$HUD.message("Tap to restart")
 			$HUD.win_message("WHITE WIN")
 		if Input.is_action_just_pressed("ui_select"):
+			$Select_sound.play()
 			game_state = START
 
 func _on_HUD_exchange():
 	if game_state == PLAYING:
+		$Change_sound.play()
 		if $Black:
 			$Black.exchange()
 		if $White:
